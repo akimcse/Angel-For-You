c2d2a720fc7087fd460c413cc979763f1a01554e
Hyuna <akim.cse@ewhain.net>
2020-10-23 00:07:51 +0900

    Updates

---
+++ precache-manifest.7e5eeba0f0bb4c5a248849153cfa7742.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1bd27c8d83d90ba0ac952f3912b2f0",
+    "url": "/angel_in_us/index.html"
+  },
+  {
+    "revision": "ec44e2f3d7f2a71aac72",
+    "url": "/angel_in_us/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f1353be03f8c5218b316",
+    "url": "/angel_in_us/static/js/2.4ebca246.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/angel_in_us/static/js/2.4ebca246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec44e2f3d7f2a71aac72",
+    "url": "/angel_in_us/static/js/main.bcae5516.chunk.js"
+  },
+  {
+    "revision": "c3a17cd8e214ce0cf757",
+    "url": "/angel_in_us/static/js/runtime-main.fa0b5d5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/angel_in_us/static/media/logo.5d5d9eef.svg"
+  }
+]);
