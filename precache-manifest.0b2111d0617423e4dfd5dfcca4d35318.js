b7aaad061e544a8ce8ccd4e862f59c84a9597c8e
Hyuna <akim.cse@ewhain.net>
2020-10-23 00:12:40 +0900

    Updates

--- precache-manifest.0b2111d0617423e4dfd5dfcca4d35318.js
+++ precache-manifest.0b2111d0617423e4dfd5dfcca4d35318.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b1bd27c8d83d90ba0ac952f3912b2f0",
+    "revision": "2e5be49b9fbc4e139f7c86e9f97f7e81",
     "url": "/angel_in_us/index.html"
   },
   {
